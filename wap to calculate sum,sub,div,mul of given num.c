c3f64fb26484ae27d6c7cb616e511b502e4da792
Sheryasikder123 <144677089+Sheryasikder123@users.noreply.github.com>
2023-11-18 22:27:11 +0530

    Create wap to calculate sum,sub,div,mul of given num.c

---
+++ wap to calculate sum,sub,div,mul of given num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    printf("enter two number:");
+    scanf ("%d%d",&a,&b);
+    printf("sum:%d\n",a+b);
+    printf("sub:%d\n",a-b);
+    printf("mul:%d\n",a*b);
+    printf("div:%d\n",a/b);
+    return 0;
+}
