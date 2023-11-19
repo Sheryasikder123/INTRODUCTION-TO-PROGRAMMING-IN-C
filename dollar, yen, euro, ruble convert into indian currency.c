7c082725772921e2b3db17b9670e886f6bec2148
Sheryasikder123 <144677089+Sheryasikder123@users.noreply.github.com>
2023-11-19 13:09:57 +0530

    Create dollar, yen, euro, ruble convert into indian currency.c

---
+++ dollar, yen, euro, ruble convert into indian currency.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+     float rs;
+     printf("enter the amount in RS.:");
+     scanf("%f",&rs);
+     printf("rupees to dollar is:%f\n",rs*0.012);
+    printf("rupees to yen is:%f\n",rs*1.76);
+    printf("rupees to euro is:%f\n",rs*0.011);
+    printf("rupees to ruble is:%f\n",rs*1.16);
+
+}
