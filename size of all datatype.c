3cf1ab95b9754179982d67ac46258db6fe86c6af
Sheryasikder123 <144677089+Sheryasikder123@users.noreply.github.com>
2023-11-27 22:34:01 +0530

    Create size of all datatype.c

---
+++ size of all datatype.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int i;
+    double d;
+    float f;
+    char c;
+    printf("the size of integer datatype is: %d\n", sizeof (i));
+    printf("the size of double datatype is: %d\n", sizeof (d));
+    printf("the size of float datatype is: %d\n", sizeof (f));
+    printf("the size of character datatype is: %d\n", sizeof (c));
+}
